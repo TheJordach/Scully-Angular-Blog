b71168774e32d326a98214ba7b4ebd2a0e5c2bdf
Jordach Makaya <makayajordach@gmail.com>
2024-06-15 02:54:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {SharedModule} from "./shared/shared.module";
+import {ContactModule} from "./contact/contact.module";
+import {ArticlesModule} from "./articles/articles.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ArticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
